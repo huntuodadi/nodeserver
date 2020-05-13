5844c6cd77b411fd15eca7d3afc091bff9a5abe1
zhangzy <490733118@qq.com>
2020-05-14 00:13:22 +0800

    session

--- server.js
+++ server.js
@@ -2,19 +2,61 @@ const http = require('http');
 const url = require('url');
 const querystring = require('querystring');
 const parseCookie = require('./parseCookie');
-console.log(parseCookie)
+
+const sessions = {};
+const key = 'session_id';
+var EXPIRES = 5 * 1000;
+const generate = () => {
+  const session = {};
+  session.id = (new Date()).getTime() + Math.random();
+  session.cookie = {
+    expire: (new Date()).getTime() + EXPIRES
+  };
+  sessions[session.id] = session;
+  return session;
+}
+
 http.createServer((req, res) => {
   const query = url.parse(req.url, true).query;
   req.query = query;
-  console.log('query:', query);
   const cookies = parseCookie(req.headers.cookie);
   req.cookies = cookies;
+  // 检查更新session
+  const id = req.cookies[key];
+  if(!id) {
+    req.session = generate();
+  }else {
+    const session = sessions[id];
+    if(session) {
+      if(session.cookie.expire > (new Date()).getTime()) {
+        // 没过期 更新session时间 
+        session.cookie.expire = (new Date()).getTime() + EXPIRES;
+        req.session = session;
+      }else {
+        delete sessions[id];
+        req.session = generate();
+      }
+    }else {
+      req.session = generate();
+    }
+  }
+
+  const writeHead = res.writeHead;
+  res.writeHead = () => {
+    let cookies = res.getHeader('Set-Cookie');
+    const session = serialize(key, req.session.id);
+    console.log('session:', session);
+    cookies = Array.isArray(cookies) ? cookies.concat(session) : [cookies, session];
+    console.log(cookies);
+    res.setHeader('Set-Cookie', cookies);
+    return writeHead.apply(this, arguments);
+  }
   handle(req, res);
 }).listen(1337)
 
 function handle(req, res) {
-  if(!req.cookies.isVisit) {
-    res.setHeader('Set-Cookie', serialize('isVisit', '1', {maxAge: 5}));
+  if(!req.session.isVisit) {
+    // res.session.isVisit = true;
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('first zoo');
   }else {
